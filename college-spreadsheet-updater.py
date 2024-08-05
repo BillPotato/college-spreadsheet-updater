71ba85cedd8af5135ab0f35aaaeca8d87c8743a7
Bill Nguyen <nbpntp@gmail.com>
2024-08-05 20:07:40 +0700

    Put match-case section into function & Added rudimentary FIELDS selection in confiq.py.

--- college-spreadsheet-updater.py
+++ college-spreadsheet-updater.py
@@ -1,18 +1,16 @@
 #!python 3
 
 # Info that can be updated:
-# school population, location, overall ranking, setting, acceptance rate, 50% top SAT score
+# school population, location, overall ranking, setting, acceptance rate
 
 import ezsheets, requests, time
 from bs4 import BeautifulSoup
 from threading import Thread
-from spreadsheet_id import spreadsheet_id
-# from time import sleep
-# from pprint import pformat
+from confiq import spreadsheet_id, FIELDS
+
 
 # prevent blocking
 newheaders = {
-#     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0'
     'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64)'
 }
 
@@ -32,7 +30,6 @@ class ThreadWithReturnValue(Thread):
         return self._return
 
 # Fetch spreadsheet
-# sheet_url = "1x0DQCtAvl6M-lL8VvZKF6zqbe2pZLJ6HqrFcfXi-YYY"
 def get_spreadsheet_rows(sheet_id):
     print("Fetching spreadsheet...")
     spreadsheet = ezsheets.Spreadsheet(sheet_id)[0]
@@ -47,17 +44,14 @@ def update_spreadsheet(sheet_id, rows):
     print("Spreadsheet uploaded!")
     
 # Find where all the columns of info that can be updated are
-# sheet = spreadsheet_rows()
-# HEADERS = ["school population", "location", "overall ranking", "setting", "acceptance"]
-def generate_key_columns(sheet: list, HEADERS = ["school population", "location", "overall ranking", "setting", "acceptance"]) -> dict:
+# FIELDS = ["school population", "location", "overall ranking", "setting", "acceptance"]
+def generate_key_columns(sheet: list, FIELDS) -> dict:
     key_columns = {}
     for column, header in enumerate(sheet[0]):
-        if header.lower() in HEADERS:
+        if header.lower() in FIELDS:
             key_columns[column] = header.lower()
-#     print(f"Key columns: {key_columns}")
     return key_columns
 
-# sheet = spreadsheet_rows()
 def get_updatable_colleges_rows(sheet: list, key_columns: list[int]) -> list[int]:
     colleges = sheet[1:] #remove header row
     updatable_rows = []
@@ -70,6 +64,7 @@ def get_updatable_colleges_rows(sheet: list, key_columns: list[int]) -> list[int
             else: pass
         row +=1
     return updatable_rows
+
 
 def update_college(college, key_columns):
     
@@ -84,81 +79,57 @@ def update_college(college, key_columns):
     
     # Fetch the results page for the "{college_name}" search
     print(f"Fetching result page for {college_name}...")
-#     start = time.time()
     results_page = requests.get("https://www.usnews.com/search?q=" + college_name, headers =  newheaders)
     results_page.raise_for_status()
-#     print(round(time.time() - start, 2))
     results_soup = BeautifulSoup(results_page.text, "html.parser")
     
     # Fetch first result's link
     print(f"Fetching page for {college_name}...")
     first_result_tag = results_soup.select("a.Anchor-byh49a-0.MediaObjectBox__AnchorWrap-sc-7ytr6b-5.eMEqFO.bbyhFG")[0]
-#     start = time.time()
     college_page = requests.get(first_result_tag.get("href"), headers = newheaders)
     college_page.raise_for_status()
-#     print(round(time.time() - start, 2))
     college_soup = BeautifulSoup(college_page.text, "html.parser")
     
     # Fill in the missing columns
-#     print(f"Missing infoS: {[key_columns[i] for i in updatable_columns]}")
     for updatable_column in updatable_columns:
-#         print(f"Missing info: {key_columns[updatable_column]}")
         missing_info = key_columns[updatable_column]
-        match missing_info:
-            case "overall ranking":
-                try:
-                    selector = "span.Villain__RankingSpan-sc-8s66oj-4.fDSmVR > span"
-                    tag_text = college_soup.select(selector)[0].getText()[1:]
-                    college[updatable_column] = tag_text
-                except IndexError:
-                    college[updatable_column] = "Error: Could not find! (check school name)"
-            case "location":
-                try:
-                    selector = "span.NuggetsContainer__LocationSpan-sc-108otk5-0.GXzCk.mr2"
-                    tag_text = college_soup.select(selector)[0].getText()
-                    tag_text = tag_text[tag_text.find("•")+2:]
-                    college[updatable_column] = tag_text
-                    # To-do: Translate state Abbreviation to Full Name
-                except IndexError:
-                    college[updatable_column] = "Error: Could not find! (check school name)"
-            case "setting":
-                try:
-                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-                    tag_text = college_soup.select(selector)[0].getText()
-                    college[updatable_column] = tag_text
-                except IndexError:
-                    college[updatable_column] = "Error: Could not find! (check school name)"
-            case "school population":
-                try:
-                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-                    tag_text = college_soup.select(selector)[2].getText()
-                    college[updatable_column] = tag_text
-                except IndexError:
-                    college[updatable_column] = "Error: Could not find! (check school name)"
-            case "acceptance":
-                try:
-                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-                    tag_text = college_soup.select(selector)[3].getText()
-                    college[updatable_column] = tag_text
-                except IndexError:
-                    college[updatable_column] = "Error: Could not find! (check school name)"
-    # print(college)       
+        try:
+            college[updatable_column] = get_info(missing_info, college_soup)
+        except IndexError:
+            college[updatable_column] = "Error: Could not find! (check school name)"     
     return college
+
+
+def get_info(missing_info: str, college_soup) -> str:
+    match missing_info:
+        case "overall ranking":
+            selector = "span.Villain__RankingSpan-sc-8s66oj-4.fDSmVR > span"
+            tag_text = college_soup.select(selector)[0].getText()[1:]
+        case "location":
+            selector = "span.NuggetsContainer__LocationSpan-sc-108otk5-0.GXzCk.mr2"
+            tag_text = college_soup.select(selector)[0].getText()
+            tag_text = tag_text[tag_text.find("•")+2:]
+        case "setting":
+            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+            tag_text = college_soup.select(selector)[0].getText()
+        case "school population":
+            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+            tag_text = college_soup.select(selector)[2].getText()
+        case "acceptance":
+            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+            tag_text = college_soup.select(selector)[3].getText()
+    
+    return tag_text
         
     
 def main():
-#     spreadsheet_id = [REDACTED]
-    
     spreadsheet_rows = get_spreadsheet_rows(spreadsheet_id)
-    key_columns = generate_key_columns(spreadsheet_rows)
-#     print(f"Key columns: {key_columns}")
+    key_columns = generate_key_columns(spreadsheet_rows, FIELDS)
     updatable_rows = get_updatable_colleges_rows(spreadsheet_rows, key_columns)
-#     print(f"Updatable rows: {updatable_rows}")
     
     # start fetching information
     college_threads = []
     for row in updatable_rows:
-#         sleep(0.3)
         college_thread = ThreadWithReturnValue(target=update_college, args=[spreadsheet_rows[row], key_columns])
         college_threads.append(college_thread)
         college_thread.start()
@@ -168,14 +139,8 @@ def main():
         spreadsheet_rows[row] = college_threads[thread_idx].join()
         thread_idx +=1
     
-    # print(f"Threads: {college_threads}")
-    
-#     with open("rows.py", "w") as file:
-#         file.write(pformat(spreadsheet_rows))
-    
     update_spreadsheet(spreadsheet_id, spreadsheet_rows)
     
     
 if __name__ == "__main__":
     main()
-
---
+++ confiq.py
@@ -0,0 +1,10 @@
+spreadsheet_id = "<INSERT SPREADSHEET ID/LINK HERE>"
+
+# Replace <INSERT SPREADSHEET ID/LINK HERE> with 
+# your spreadsheet's ID/LINK
+# example: spreadsheet_id = "1XZ4ruaR8mLOJIi7PIUvorb1BPy9tu1-M963MpIFO6lA"
+# Remember to keep the quotes ("")
+
+FIELDS = ["school population", "location", "overall ranking", "setting", "acceptance"]
+
+# Select statistics you want to track from: ["school population", "location", "overall ranking", "setting", "acceptance"]
