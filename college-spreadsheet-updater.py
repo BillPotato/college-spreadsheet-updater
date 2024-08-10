ce8d4c460708e6cd32ae494cccec89f98fd697d9
Bill Nguyen <nbpntp@gmail.com>
2024-08-10 15:02:08 +0700

    Converted program into Object-Oriented-Programming + Added empty school name detection + Added  0.3-0.5 second ban prevention.

--- college-spreadsheet-updater.py
+++ college-spreadsheet-updater.py
@@ -6,163 +6,175 @@
 import ezsheets, requests
 from bs4 import BeautifulSoup
 from threading import Thread
+import time
+import random
+import re
 from tqdm import tqdm
-from json.decoder import JSONDecodeError # Error handling
+from json.decoder import JSONDecodeError # For error handler
 import confiq # File
-from state_abbreviations import state_abbreviations_dict # File
+import state_abbreviations # File
 
-# prevent blocking
+# Prevent blocking
 newheaders = {
     'User-Agent': 'Mozilla/5.0 (X11; Linux i686 on x86_64)'
-    # Alternative: 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0'
+#     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:121.0) Gecko/20100101 Firefox/121.0'
 }
 
-# Custom threading class to return value
-class ThreadWithReturnValue(Thread):
+
+class college_sheet:
     
-    def __init__(self, group=None, target=None, name=None,
-                 args=(), kwargs={}, Verbose=None):
-        Thread.__init__(self, group, target, name, args, kwargs)
-        self._return = None
-
-    def run(self):
-        if self._target is not None:
-            self._return = self._target(*self._args, **self._kwargs)
-    def join(self, *args):
-        Thread.join(self, *args)
-        return self._return
-
-# Fetch spreadsheet
-def get_spreadsheet_rows(sheet_id):
-    print("Fetching spreadsheet...")
-    spreadsheet = ezsheets.Spreadsheet(sheet_id)[0]
-    rows = spreadsheet.getRows()
-    print("Fetched spreadsheet...")
-    return rows
-
-def update_spreadsheet(sheet_id, rows):
-    print("Uploading spreadsheet...")
-    spreadsheet = ezsheets.Spreadsheet(sheet_id)[0]
-    spreadsheet.updateRows(rows)
-    print("Spreadsheet uploaded!")
+    def __init__(self, sheet_id):
+        self.sheet_id: str = sheet_id
+        self.spreadsheet_obj = ezsheets.Spreadsheet(sheet_id)[0]
+        self.rows: list = self.spreadsheet_obj.getRows()
+        self.college_name_column = self.spreadsheet_obj.getColumn(1) # don't remove header
+        self.key_columns: dict[int, str] = {}
+        self.unfilled_colleges: list[int] = []
+        self.college_threads: list = []
+        
+        
+    def upload_spreadsheet(self):
+        
+        print("Uploading spreadsheet...")
+        self.spreadsheet_obj.updateRows(self.rows)
+        print("Spreadsheet uploaded!")
     
-# Find where all the columns of info that can be updated are
-# FIELDS = "school population", "location", "overall ranking", "setting", "acceptance", "sat", "act", "gpa"]
-def generate_key_columns(sheet: list, FIELDS) -> dict:
-    key_columns = {}
-    for column, header in enumerate(sheet[0]):
-        if header.lower() in FIELDS:
-            key_columns[column] = header.lower()
-    return key_columns
-
-def get_updatable_college_rows(sheet: list, key_columns: dict[int, str]) -> list[int]:
-    colleges = sheet[1:] #remove header row
-    updatable_rows = []
-    row = 0 # start at row 1 (row 0 is header)
-    while colleges[row][0]:
-        for column in key_columns:
-            if not colleges[row][column]: # if missing column -> college is updatable
-                updatable_rows.append(row+1) # +1 because removed first row
-                break
+    def generate_key_columns(self, FIELDS: list[str]):
+        headers = self.rows[0]
+        for column, header in enumerate(headers):
+            if header.lower() in FIELDS:
+                self.key_columns[column] = header.lower()
             else: pass
-        row +=1
-    return updatable_rows
 
-
-def update_college(college, key_columns):
+    def get_unfilled_college_rows(self):
+        
+        for row, college_name in enumerate(self.college_name_column[1:]):
+            if not(re.compile(r"^(\s)*$").search(college_name)):
+            # ^checks if there are characters other than space/tab/newline in string
+                college_obj = college(row +1, self.rows[row +1])
+                
+                if not college_obj.is_filled(self.key_columns):
+                    self.unfilled_colleges.append(college_obj)
+                else: pass # college is filled
+            else: pass # no college to check
+                  
+        
+    def fill_unfilled_colleges(self):
+        college_updating_threads = []
+        bar_format = "{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} {unit}"
+        
+        for college in tqdm(self.unfilled_colleges, desc="Creating threads", unit="threads", bar_format=bar_format):
+            time.sleep(random.uniform(0.3, 0.5))
+            college_updating_thread = Thread(target=college.update, args=[self.key_columns])
+            college_updating_threads.append(college_updating_thread)
+            college_updating_thread.start()
+        
+        for college_updating_thread in tqdm(college_updating_threads, desc="Updating colleges", unit="colleges", bar_format=bar_format):
+            college_updating_thread.join()
+            
+        for college in tqdm(self.unfilled_colleges, desc="Applying changes", unit="colleges", bar_format=bar_format):
+            self.rows[college.row_idx] = college.row
+        
+class college:
     
-    college_name = college[0]
-    
-    # Find missing KEY columns
-    updatable_columns = []
-    for column in key_columns:
-        if not college[column]:
-            updatable_columns.append(column)
-        else: pass
-    
-    # Fetch the results page for the "{college_name}" search
-    results_page = requests.get("https://www.usnews.com/search?q=" + college_name, headers =  newheaders)
-    results_page.raise_for_status()
-    results_soup = BeautifulSoup(results_page.text, "html.parser")
-    
-    # Fetch first result's link
-    first_result_tag = results_soup.select("a.Anchor-byh49a-0.MediaObjectBox__AnchorWrap-sc-7ytr6b-5.eMEqFO.bbyhFG")[0]
-    college_page = requests.get(first_result_tag.get("href"), headers = newheaders)
-    college_page.raise_for_status()
-    college_soup = BeautifulSoup(college_page.text, "html.parser")
-    
-    # Fill in the missing columns
-    for updatable_column in updatable_columns:
-        missing_info = key_columns[updatable_column]
-        try:
-            college[updatable_column] = get_info(missing_info, college_soup)
-        except IndexError:
-            college[updatable_column] = "Error: Could not find! (check school name)"     
-    return college
-
-
-def get_info(missing_info: str, college_soup) -> str: # college_soup is of type BeautifulSoup
-    
-    tag_text = "Error: Could not match missing_info! (Please report to dev)"
-    # Error handler (just in case)
-    
-    match missing_info:
-        case "overall ranking":
-            selector = "span.Villain__RankingSpan-sc-8s66oj-4.fDSmVR > span"
-            tag_text = college_soup.select(selector)[0].getText()[1:]
-        case "location":
-            selector = "span.NuggetsContainer__LocationSpan-sc-108otk5-0.GXzCk.mr2"
-            tag_text = college_soup.select(selector)[0].getText()
-            tag_text = tag_text[tag_text.find("•")+2:]
-            state_portion = tag_text[-2:]
-            tag_text = tag_text.replace(state_portion, state_abbreviations_dict[state_portion])
-        case "setting":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[0].getText()
-        case "school population":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[2].getText()
-        case "acceptance":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[3].getText()
-        case "sat":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[4].getText()
-        case "act":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[5].getText()
-        case "gpa":
-            selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
-            tag_text = college_soup.select(selector)[6].getText()
-    return tag_text
+    def __init__(self, row_idx: int, row: list):
+        self.row_idx: int = row_idx # index in college_sheet.rows
+        self.row: list = row
+        self.name = self.row[0]
+        self.unfilled_cells: list[int] = []
         
     
+    def is_filled(self, key_columns: dict[int, str]): # This func also modifies self.unfilled_cells
+        filled_status = True
+        for column in key_columns:
+            if not self.row[column]:
+                self.unfilled_cells.append(column)
+                filled_status = False
+            else: pass
+        return filled_status
+        
+    def update(self, key_columns): # Run is_filled first
+        
+        def get_stat(missing_stat: str, college_soup): # college_soup is of type BeautifulSoup
+            
+            tag_text = "Error: Could not match missing_stat! (Please report to dev)"
+            # Error handler (just in case)
+            
+            match missing_stat:
+                case "overall ranking":
+                    selector = "span.Villain__RankingSpan-sc-8s66oj-4.fDSmVR > span"
+                    tag_text = college_soup.select(selector)[0].getText()[1:]
+                case "location":
+                    selector = "span.NuggetsContainer__LocationSpan-sc-108otk5-0.GXzCk.mr2"
+                    tag_text = college_soup.select(selector)[0].getText()
+                    tag_text = tag_text[tag_text.find("•")+2:]
+                    state_portion = tag_text[-2:]
+                    tag_text = tag_text.replace(state_portion, state_abbreviations.state_abbreviations_dict[state_portion])
+                case "setting":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[0].getText()
+                case "school population":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[2].getText()
+                case "acceptance":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[3].getText()
+                case "sat":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[4].getText()
+                case "act":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[5].getText()
+                case "gpa":
+                    selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
+                    tag_text = college_soup.select(selector)[6].getText()
+            return tag_text
+        
+        
+        # Fetch result page for college
+#         print(f"Fetching result page for {self.name}...")
+        results_page = requests.get("https://www.usnews.com/search?q=" + self.name, headers =  newheaders)
+        results_page.raise_for_status()
+        results_soup = BeautifulSoup(results_page.text, "html.parser")
+        
+        # Fetch page for college
+#         print(f"Fetching page for {self.name}...")
+        first_result_tag = results_soup.select("a.Anchor-byh49a-0.MediaObjectBox__AnchorWrap-sc-7ytr6b-5.eMEqFO.bbyhFG")[0]
+        college_page = requests.get(first_result_tag.get("href"), headers = newheaders)
+        college_page.raise_for_status()
+        college_soup = BeautifulSoup(college_page.text, "html.parser")
+        
+        # Fill unfilled cells
+        for unfilled_cell in self.unfilled_cells:
+            unfilled_stat_header = key_columns[unfilled_cell]
+            try:
+                self.row[unfilled_cell] = get_stat(unfilled_stat_header, college_soup)
+            except IndexError as Error:
+                self.row[unfilled_cell] = f"{Error}: Could not find school! (check name)"
+            # Error handler
+        
+
 def main():
-    try:
-        spreadsheet_rows = get_spreadsheet_rows(confiq.spreadsheet_id)
-    except JSONDecodeError as e:
-        print(f"Error: {e} while fetching. Try again!")
-        
-    key_columns = generate_key_columns(spreadsheet_rows, confiq.FIELDS)
-    updatable_rows = get_updatable_college_rows(spreadsheet_rows, key_columns)
-    
-    # start fetching information
-    college_threads = []
-    for row in updatable_rows:
-        college_thread = ThreadWithReturnValue(target=update_college, args=[spreadsheet_rows[row], key_columns])
-        college_threads.append(college_thread)
-        college_thread.start()
-    
-    bar_format = "{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} {unit}"
-    for thread_idx, row in enumerate(tqdm(updatable_rows, desc="Updating colleges", unit="colleges", bar_format=bar_format)):
-        spreadsheet_rows[row] = college_threads[thread_idx].join()
-        thread_idx +=1
-
-    try:
-        update_spreadsheet(confiq.spreadsheet_id, spreadsheet_rows)
-    except JSONDecodeError as e:
-        print(f"Error: {e} while uploading. Try again!")
-    
+    college_sheet_obj = college_sheet(confiq.spreadsheet_id)
+    college_sheet_obj.generate_key_columns(confiq.FIELDS)
+    college_sheet_obj.get_unfilled_college_rows()
+    college_sheet_obj.fill_unfilled_colleges()
+    college_sheet_obj.upload_spreadsheet()
     
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except JSONDecodeError as Error:
+        print(f"{Error}: Try again!")
+    # Error handler
+    
+                
+                
+        
+        
+    
+    
+        
+        
+        
+        
