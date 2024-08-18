898a8fec4c1103117d7b59d03058770da9c53c36
Bill Nguyen <nbpntp@gmail.com>
2024-08-18 10:10:25 +0700

    Added format validation for missing stats + Added custom type annotations.

--- college-spreadsheet-updater.py
+++ college-spreadsheet-updater.py
@@ -3,16 +3,19 @@
 # Stats that can be updated:
 # "school population", "location", "overall ranking", "setting", "acceptance", "sat", "act", "gpa"
 
-import ezsheets, requests
+import requests
+import ezsheets
+import time
+import re
+import random
+from typing import Type
 from bs4 import BeautifulSoup
 from threading import Thread
-import time
-import random
-import re
 from tqdm import tqdm
 from json.decoder import JSONDecodeError # For error handler
 import confiq # File
 import state_abbreviations # File
+
 
 # Prevent blocking
 newheaders = {
@@ -25,12 +28,12 @@ class college_sheet:
     
     def __init__(self, sheet_id):
         self.sheet_id: str = sheet_id
-        self.spreadsheet_obj = ezsheets.Spreadsheet(sheet_id)[0]
-        self.rows: list = self.spreadsheet_obj.getRows()
+        self.spreadsheet_obj: Type[ezsheets.Sheet] = ezsheets.Spreadsheet(sheet_id)[0]
+        self.rows: list[list] = self.spreadsheet_obj.getRows()
         self.college_name_column = self.spreadsheet_obj.getColumn(1) # don't remove header
         self.key_columns: dict[int, str] = {}
-        self.unfilled_colleges: list[int] = []
-        self.college_threads: list = []
+        self.unfilled_colleges: list[college] = []
+        self.college_threads: list[Type[Thread]] = []
         
         
     def upload_spreadsheet(self):
@@ -47,7 +50,7 @@ class college_sheet:
             else: pass
 
     def get_unfilled_college_rows(self):
-        
+
         for row, college_name in enumerate(self.college_name_column[1:]):
             if not(re.compile(r"^(\s)*$").search(college_name)):
             # ^checks if there are characters other than space/tab/newline in string
@@ -57,14 +60,14 @@ class college_sheet:
                     self.unfilled_colleges.append(college_obj)
                 else: pass # college is filled
             else: pass # no college to check
-                  
+
         
     def fill_unfilled_colleges(self):
         college_updating_threads = []
         bar_format = "{desc}: {percentage:3.0f}%|{bar}| {n_fmt}/{total_fmt} {unit}"
         
         for college in tqdm(self.unfilled_colleges, desc="Creating threads", unit="threads", bar_format=bar_format):
-            time.sleep(random.uniform(0.3, 0.5))
+            time.sleep(random.uniform(0.1, 0.3))
             college_updating_thread = Thread(target=college.update, args=[self.key_columns])
             college_updating_threads.append(college_updating_thread)
             college_updating_thread.start()
@@ -80,7 +83,7 @@ class college:
     def __init__(self, row_idx: int, row: list):
         self.row_idx: int = row_idx # index in college_sheet.rows
         self.row: list = row
-        self.name = self.row[0]
+        self.name: str = self.row[0]
         self.unfilled_cells: list[int] = []
         
     
@@ -104,30 +107,40 @@ class college:
                 case "overall ranking":
                     selector = "span.Villain__RankingSpan-sc-8s66oj-4.fDSmVR > span"
                     tag_text = college_soup.select(selector)[0].getText()[1:]
+                    if not tag_text.isdigit(): tag_text = "N/A"
                 case "location":
                     selector = "span.NuggetsContainer__LocationSpan-sc-108otk5-0.GXzCk.mr2"
                     tag_text = college_soup.select(selector)[0].getText()
                     tag_text = tag_text[tag_text.find("•")+2:]
                     state_portion = tag_text[-2:]
-                    tag_text = tag_text.replace(state_portion, state_abbreviations.state_abbreviations_dict[state_portion])
+                    try:
+                        tag_text = tag_text.replace(state_portion, state_abbreviations.state_abbreviations_dict[state_portion])
+                    except KeyError:
+                        tag_text = "N/A"
                 case "setting":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[0].getText()
+                    if not tag_text.lower() in ["city", "urban", "suburban", "rural"]: tag_text = "N/A"
                 case "school population":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[2].getText()
+                    if not (tag_text[0].isdigit() and tag_text[-1].isdigit()): tag_text = "N/A"                        
                 case "acceptance":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[3].getText()
+                    if tag_text[-1] != "%": tag_text = "N/A"
                 case "sat":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[4].getText()
+                    if "-" not in tag_text: tag_text = "N/A"
                 case "act":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[5].getText()
+                    if "-" not in tag_text: tag_text = "N/A"
                 case "gpa":
                     selector = "p.Paragraph-sc-1iyax29-0.kqzqfx"
                     tag_text = college_soup.select(selector)[6].getText()
+                    if "." not in tag_text: tag_text = "N/A"
             return tag_text
         
         
@@ -150,7 +163,7 @@ class college:
             try:
                 self.row[unfilled_cell] = get_stat(unfilled_stat_header, college_soup)
             except IndexError as Error:
-                self.row[unfilled_cell] = f"{Error}: Could not find school! (check name)"
+                self.row[unfilled_cell] = f"Error: {Error}"
             # Error handler
         
 
@@ -166,15 +179,4 @@ if __name__ == "__main__":
         main()
     except JSONDecodeError as Error:
         print(f"{Error}: Try again!")
-    # Error handler
-    
-                
-                
-        
-        
-    
-    
-        
-        
-        
-        
+    # Error handler   
