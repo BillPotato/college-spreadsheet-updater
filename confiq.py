a4ea17b9c4f6c8a46e7b26095ece8f2ef11a0e8c
Bill Nguyen <nbpntp@gmail.com>
2024-08-19 12:15:35 +0700

    Converted FIELDS from list to dict + Fixed tag invalidation system.

--- confiq.py
+++ confiq.py
@@ -1,12 +1,33 @@
-spreadsheet_id = "1XZ3ruaR8mLOJIi7PIYvorb1BPy9tu1-M963MpIFO6lE"
+spreadsheet_id = "1XZ3ruaR8mLOJIi7PIYvorb1BPy9tu1-M963MpIFO6lE" # Python sheet
 
 # Replace <INSERT SPREADSHEET ID/LINK HERE> with 
 # your spreadsheet's ID/LINK
 # example: spreadsheet_id = "1XZ4ruaR8mLOJIi7PIUvorb1BPy9tu1-M963MpIFO6lA"
 # Remember to keep the quotes ("")
 
-FIELDS = ["school population", "location", "overall ranking", "setting", "acceptance", "sat", "act", "gpa"]
+# Only change keys, do NOT change values !!
+# WARNING: write everything in LOWERCASE
+FIELDS = {
+    "school population": "school population",
+    "location": "location",
+    "overall ranking": "overall ranking",
+    "setting": "setting",
+    "acceptance": "acceptance",
+    "sat": "sat",
+    "act": "act",
+    "gpa": "gpa"
+}
 
-# Select stats you want to track from below list:
-# ["school population", "location", "overall ranking", "setting", "acceptance", "sat", "act", "gpa"]
-# WARNING: write stats in **LOWERCASE**
+# Default template
+'''
+FIELDS = {
+    "school population": "school population",
+    "location": "location",
+    "overall ranking": "overall ranking",
+    "setting": "setting",
+    "acceptance": "acceptance",
+    "sat": "sat",
+    "act": "act",
+    "gpa": "gpa"
+}
+'''
